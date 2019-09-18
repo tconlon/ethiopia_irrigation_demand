8589cd92e11d309aad518052ff3f81cadf92441b
terenceconlon <tconlon10@gmail.com>
2019-09-18 10:00:17 -0400

    adding comments

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from shapely.ops import transform
 
 
 def get_args():
+    # Store all parameters for easy retrieval
     parser = argparse.ArgumentParser(
         description = 'Module for creating productive demand time series using irrigibility map')
     parser.add_argument('--params_filename',
@@ -27,16 +28,16 @@ def get_args():
     for k,v in config.items():
         args.__dict__[k] = v
 
-
-    #args.create_model_function = eval(args.create_model_function)
     return args
 
 def load_regions_shapefile(args):
+    # Load city-regions shapefiles
     shp_filename = os.path.join(args.data_dir, 'region_shapefiles', 'country_Theissen_Inters.shp')
     regions_shp = fiona.open(shp_filename)
     return regions_shp
 
 def load_eth_shapefile(args):
+    # Load shapefile for all of Ethiopia
     shp_filename = os.path.join(args.data_dir, 'eth_shapefile', 'ETH_outline.shp')
     regions_shp = fiona.open(shp_filename)
     return regions_shp
@@ -48,18 +49,23 @@ def load_irrigibility(args):
 
     filename = os.path.join(args.data_dir, 'worqlul_irrigibility', 'land_suitability.tif')
 
-    ## Determine whether you want to save updated irrigibility map
+    # Determine whether you want to save updated irrigibility map
     save_cropped = True
 
-    ## Load irrigibility tif
+    # Load irrigibility tif
     with rasterio.open(filename, 'r+') as src:
-        ## Mask based on the irrigibility threshold
+        # Threshold based on the irrigibility threshold
         cropped_src = src.read() >= args.irrigibility_lb
+
+        # Save values of thresholded irrigibility map -- more helpful than binaries
         cropped_src = np.array(cropped_src, dtype = np.uint8)
-
         metadata = src.meta
-        save_image_dest = os.path.join(args.data_dir, 'worqlul_irrigibility', 'land_suitability_cropped.tif')
 
+        # Create new save image destination
+        save_image_dest = os.path.join(args.data_dir, 'worqlul_irrigibility',
+                                       'land_suitability_cropped_irrigibility_lb_{}.tif'.format(args.irrigibility_lb))
+
+        # Save new cropped irrigibility map
         if save_cropped:
             with rasterio.open(save_image_dest, 'w+', **metadata) as dest:
                 dest.write(cropped_src)
@@ -68,12 +74,14 @@ def load_irrigibility(args):
 
 def rescale_reprojected_rainfall():
     '''
-    This rescaling method can change -- my best guess at what it could be
+    This rescaling method can change -- my best guess at what it could be.
     '''
 
+    # Find file names for original and reprojected CHIRPS rainfall images
     original    = glob.glob(args.tmp_dir + '/eth.tif')[0]
     reprojected = glob.glob(args.tmp_dir + '/eth_reproject.tif')[0]
 
+    # Load images
     with rasterio.open(original, 'r') as orig:
         with rasterio.open(reprojected, 'r') as reproj:
             orig_min, orig_max     = np.min(orig.read()),   np.max(orig.read())
@@ -81,14 +89,16 @@ def rescale_reprojected_rainfall():
 
             meta = reproj.meta
 
+            # Rescale reprojected image if necessary to rainfall maximum on the original image
             if orig_max > reproj_max:
                 reproj_array = np.maximum(reproj.read(), 0) * orig_max/reproj_max
             else:
                 reproj_array = np.maximum(reproj.read(), 0) * reproj_max / orig_max
 
-
+    # Removed reprojected, unscaled image
     os.remove(reprojected)
 
+    # Save reprojected, scaled image
     with rasterio.open(reprojected, 'w+', **meta) as new_reproj:
         new_reproj.write(reproj_array)
 
@@ -100,15 +110,17 @@ def reproject_chirps_rainfall(args, chirps_filename, irrigibility_filename, eth_
     Can also change the reprojection method
     '''
 
+    # Load Ethiopia shapefile -- Thanks Tim!
     eth_poly = Polygon(eth_shp[0]['geometry']['coordinates'][0])
 
+    # Load irrigibility map
     with rasterio.open(irrigibility_filename, 'r+') as irrig_src:
 
         dst_crs = irrig_src.meta['crs']
 
-        ## Open CHIRPS rainfall file and crop to Ethiopia
+        # Open CHIRPS rainfall file and crop to Ethiopia
         with rasterio.open(chirps_filename, 'r+') as chirps_src:
-
+            # Crop CHIRPS data to Ethiopia
             out_rainfall_image, out_rainfall_transform = mask(chirps_src, [eth_poly], crop=True)
             cropped_chirps_metadata = chirps_src.meta
             out_rainfall_height = out_rainfall_image.shape[1]
@@ -119,29 +131,36 @@ def reproject_chirps_rainfall(args, chirps_filename, irrigibility_filename, eth_
                              "crs": chirps_src.crs
                              })
 
-            ## Save cropped Ethiopia CHIRPS data
+            # Save cropped Ethiopia CHIRPS data
             tmp_save_low_res = os.path.join(args.data_dir, 'chirps', 'tmp', 'eth.tif')
             with rasterio.open(tmp_save_low_res, 'w', **cropped_chirps_metadata) as eth_chirps:
                 eth_chirps.write(out_rainfall_image)
-                # os.remove(tmp_save_low_res)
 
+                # Copy the meta data for the irrigiblity map
                 kwargs = irrig_src.meta.copy()
+
+                # Set the rainfall data type to float
                 kwargs['dtype'] = chirps_src.meta['dtype']
+
+                # Create temporary save for reporjected CHIRPS tif
                 tmp_save = tmp_save_low_res[0:-4] + '_reproject.tif'
 
-                ## Reproject and upsample Ethiopia CHIRPS data
+                ## Reproject and upsample Ethiopia CHIRPS data (this also saves the image)
                 with rasterio.open(tmp_save, 'w+', **kwargs) as reproj_dst:
                     reproject(
                         source        = rasterio.band(eth_chirps, 1),
                         destination   = rasterio.band(reproj_dst, 1),
+                        # Resampling method: this can change! Really don't know what would be best
                         resampling    = Resampling.cubic_spline)
 
-
+        # Rescale the reprojected CHIRPS file if desired
         if args.rescale_reprojected:
             rescale_reprojected_rainfall()
 
 
 def project_wsg_shape_to_csr(shape, csr):
+
+    # Need to reproject the city-region shapefiles into the same coordinate reference system as the irrigiblity map
     project = lambda x, y: pyproj.transform(
     pyproj.Proj(init='epsg:4326'),
     pyproj.Proj(init=csr),
@@ -149,17 +168,25 @@ def project_wsg_shape_to_csr(shape, csr):
     return transform(project, shape)
 
 
-def calculate_energy_deficit(args, cropped_irrig_filename, regions_shp):
+def calculate_energy_deficit(args, thresholded_irrig_filename, regions_shp):
+
+    # Find reprojected image file
     chirps_reproj = glob.glob(args.tmp_dir + '/eth_reproject.tif')[0]
 
     num_regions = len(regions_shp)
-    regional_energy_deficit = np.zeros((num_regions)) ## NOTE: For now, this is given in GWh
 
-    with rasterio.open(cropped_irrig_filename, 'r+') as irrig_src:
+    # Create array for saving model outputs. For now, this is given in GWh
+    regional_energy_deficit = np.zeros((num_regions))
+
+    # Open thresholded irrigibility map
+    with rasterio.open(thresholded_irrig_filename, 'r+') as irrig_src:
+        # Open reprojected daily CHIRPS tif
         with rasterio.open(chirps_reproj, 'r+') as chirps_src:
+            # Loop through the city-regions
             for i, j in enumerate(regions_shp):
-
+                # Load the city-region polygon
                 region_poly = Polygon(j['geometry']['coordinates'][0])
+                # Reproject the city-region polygon
                 projected_region = project_wsg_shape_to_csr(region_poly, 'epsg:32637')
 
                 # Crop reprojected CHIRPS and irrigibility map
@@ -181,9 +208,8 @@ def calculate_energy_deficit(args, cropped_irrig_filename, regions_shp):
                 # Calculate total energy required for water deficit, density = 1000 kg/m3, convert to GWh
                 total_daily_energy = (1000 * total_water_deficit_m3 * 9.81 * args.gw_depth * args.frac_irrig *
                                       1/args.w2w_eff) / (1e9 * 3600)
-                # total_avg_power = total_daily_energy / 24 # GW
 
-
+                # Store daily energy requirements in an array
                 regional_energy_deficit[i] = total_daily_energy
 
                 ## Can use this code to visualize areas with energy/water deficits
@@ -204,30 +230,42 @@ def calculate_energy_deficit(args, cropped_irrig_filename, regions_shp):
 
 
 if __name__ == '__main__':
-    # get arguments from .yaml
+    # Get arguments from .yaml
     args = get_args()
 
-    # print('a')
+    # Threshold irrigibility map based on irrigibility lowerbound
+    thresholded_irrig_filename = load_irrigibility(args)
 
-    cropped_irrig_filename = load_irrigibility(args)
-
+    # Load Ethiopia shapefile
     eth_shp = load_eth_shapefile(args)
+
+    # Load city-region shapefile
     regions_shp = load_regions_shapefile(args)
+
+    # Extract city-region names from shapefile
     cities = [i['properties']['CityName'] for i in regions_shp]
-    chirps_list = []
 
-
+    # Set the CHIRPS directory name for the year in question
     chirps_dirname = os.path.join(args.chirps_dir, str(args.year))
+
+    # Sort the images so that they're in chronological order
     chirps_list = sorted(glob.glob(chirps_dirname + '/*.tif'))
+
+    # Create dataframe for exporting results
     exports_df = pd.DataFrame(index = range(365), columns=cities)
 
-
-    total_energy_deficit_timeseries = np.zeros((len(chirps_list), len(regions_shp)))
-
+    # Loop through all the CHIRPS files in the directory
     for i, chirps_filename in enumerate(chirps_list):
+        # Print what number file you're currently on -- sanity check
         print(i)
-        reproject_chirps_rainfall(args, chirps_filename, cropped_irrig_filename, eth_shp)
-        regional_energy_deficit = calculate_energy_deficit(args, cropped_irrig_filename, regions_shp)
+
+        # Reproject the CHIRPS tif
+        reproject_chirps_rainfall(args, chirps_filename, thresholded_irrig_filename, eth_shp)
+
+        # Calculate city-regional energy deficit
+        regional_energy_deficit = calculate_energy_deficit(args, thresholded_irrig_filename, regions_shp)
+
+        # Store energy deficit calculations for export
         exports_df.iloc[i, : ] = regional_energy_deficit
 
         # Clean up tmp dir
@@ -235,8 +273,11 @@ if __name__ == '__main__':
         for f in tmp_files:
             os.remove(f)
 
+    # Create filename for results export
     out_file = 'irrig_elec_results_year_{}_irriglb_{}_h20req_{}.csv'.format(args.year,
                                                                             args.irrigibility_lb, args.h20_req)
     outpath = os.path.join(args.elec_results_dir, out_file)
+
+    # Export results!
     exports_df.to_csv(outpath)
 
